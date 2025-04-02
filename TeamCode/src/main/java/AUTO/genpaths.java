1b1a9a8e8968e44b052e470fd56e420c436a2595
chriswn <onichr1074@apps.district279.org>
2025-04-01 22:52:02 -0500

    made test code using the sample and made my own paths in "genpaths";

---
+++ TeamCode/src/main/java/AUTO/genpaths.java
@@ -0,0 +1,82 @@
+package AUTO;
+
+public class genpaths {
+
+//    public class GeneratedPaths {
+//
+//        public static PathBuilder builder = new PathBuilder();
+//
+//        public static PathChain line1 = builder
+//                .addPath(
+//                        new BezierLine(
+//                                new Point(4.854, 2.225, Point.CARTESIAN),
+//                                new Point(42.472, 2.022, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .build();
+//
+//        public static PathChain line2 = builder
+//                .addPath(
+//                        new BezierCurve(
+//                                new Point(42.472, 2.022, Point.CARTESIAN),
+//                                new Point(5.056, 5.663, Point.CARTESIAN),
+//                                new Point(4.854, 129.034, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .build();
+//
+//        public static PathChain line3 = builder
+//                .addPath(
+//                        new BezierLine(
+//                                new Point(4.854, 129.034, Point.CARTESIAN),
+//                                new Point(43.079, 12.337, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .build();
+//
+//        public static PathChain line4 = builder
+//                .addPath(
+//                        new BezierCurve(
+//                                new Point(43.079, 12.337, Point.CARTESIAN),
+//                                new Point(16.382, 33.169, Point.CARTESIAN),
+//                                new Point(8.292, 129.438, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .build();
+//
+//        public static PathChain line5 = builder
+//                .addPath(
+//                        new BezierLine(
+//                                new Point(8.292, 129.438, Point.CARTESIAN),
+//                                new Point(43.685, 22.652, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .build();
+//
+//        public static PathChain line6 = builder
+//                .addPath(
+//                        new BezierCurve(
+//                                new Point(43.685, 22.652, Point.CARTESIAN),
+//                                new Point(56.427, 57.438, Point.CARTESIAN),
+//                                new Point(13.753, 133.281, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .build();
+//
+//        public static PathChain line7 = builder
+//                .addPath(
+//                        new BezierLine(
+//                                new Point(13.753, 133.281, Point.CARTESIAN),
+//                                new Point(6.472, 4.449, Point.CARTESIAN)
+//                        )
+//                )
+//                .setTangentHeadingInterpolation()
+//                .build();
+//    }
+}
